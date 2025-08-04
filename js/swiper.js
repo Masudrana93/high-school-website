3401d99b040a087256ed5a83e19f9c2841da8d4a
masudrana93 <mr007masudrana@gmail.com>
2025-08-04 18:42:00 +0600

    updated curriculam and other activities slider

--- js/swiper.js
+++ js/swiper.js
@@ -21,3 +21,25 @@
       992: { slidesPerView: 3 }
     }
   });
+
+  // --For Curriculam and other activites Swiper --
+ 
+    const swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: ".first-slider-button-next",
+        prevEl: ".first-slider-button-prev",
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      576: { slidesPerView: 2 },
+      768: { slidesPerView: 3 },
+      992: { slidesPerView: 4 }
+    }
+  });
