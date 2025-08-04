e6dfc9c70c7a3bc7135beeb78159a5c7b7dcb46b
masudrana93 <mr007masudrana@gmail.com>
2025-08-04 15:31:08 +0600

    updated gallery slider

---
+++ js/swiper.js
@@ -0,0 +1,23 @@
+
+
+// --For Gallery Swiper --
+ 
+    const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      576: { slidesPerView: 2 },
+      768: { slidesPerView: 3 },
+      992: { slidesPerView: 3 }
+    }
+  });
